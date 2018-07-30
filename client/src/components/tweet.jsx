4cdb446c386a1b6049a73a2fedb08fd99278804a
Vigneshwaran <vigneshwaran.veeran@sap.com>
2018-07-30 01:31:45 -0400

    tweet updated

--- client/src/components/tweet.jsx
+++ client/src/components/tweet.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import "../styles/tweet.css";
 
 class Tweet extends Component {
   state = {};
   render() {
-    return <div>Tweet</div>;
+    return (
+      <React.Fragment>
+        <div class="tweet-container">
+          <div class="avatar">
+            <img src="https://pbs.twimg.com/profile_images/890252563190226944/lP9wOBFy_normal.jpg" />
+          </div>
+          <div class="data">
+            <div class="header">TechCrunch 7/29/2018, 11:22:03 PM</div>
+            <div class="content">
+              Shortcuts will play a major role in making Siri a truly useful
+              assistant https://t.co/3fQJGI9f6V
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
