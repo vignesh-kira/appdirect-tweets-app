b67d780902c2a5d4cdfd88802c8e18bb395283bd
Vigneshwaran <vigneshwaran.veeran@sap.com>
2018-07-29 22:42:02 -0400

    restructred app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./header";
 
 class App extends Component {
   state = {
@@ -22,18 +21,7 @@ class App extends Component {
     return body;
   };
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.{" "}
-          {this.state.response}
-        </p>
-      </div>
-    );
+    return <Header />;
   }
 }
 
---
+++ client/src/components/header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import "../styles/App.css";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Tweets Display Demo</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+// {this.state.response}
+
+export default Header;
