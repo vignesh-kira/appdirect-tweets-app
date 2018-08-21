6e9fc2a1486793a50a0afad481620eac4dc7e2f4
Veeran <vigneshwaran.veeran@sap.com>
2018-08-21 14:14:30 -0400

    removed cache and ignored node modules

--- node_modules/twitter-proxy/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Grab this info from your app page on dev.twitter.com.
-  // Remember: don't share it with anyone!
-  consumerKey: '',
-  consumerSecret: '',
-  accessToken: '',
-  accessTokenSecret: ''
-};
