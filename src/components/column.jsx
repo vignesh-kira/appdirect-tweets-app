565476f54744684602ea4bba0f344d85ffe15afe
Veeran <vigneshwaran.veeran@sap.com>
2018-08-22 08:50:32 -0400

    twitter url added to props

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Tweet from "./tweet";
 import "../styles/tweet.css";
 
-const Column = ({ tweets, tweetCount }) => {
+const Column = ({ tweets, tweetCount, url }) => {
   return (
     <div className="tweet-column-data">
       {tweets.slice(0, tweetCount).map(tweet => (
-        <Tweet key={tweet.index} tweet={tweet} />
+        <Tweet key={tweet.index} tweet={tweet} url={url} />
       ))}
     </div>
   );
